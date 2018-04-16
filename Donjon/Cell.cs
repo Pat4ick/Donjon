cd2a2b0295dd817c57ecf6ebebdc16bc1a6d0da5
Patrick <lmpatrick.frank@gmail.com>
2018-04-16 14:27:45 +0200

    Position struct

--- Donjon/Cell.cs
+++ Donjon/Cell.cs
@@ -2,5 +2,11 @@
 {
     internal class Cell
     {
+        public Position Position { get; private set; }
+
+        public Cell(Position position)
+        {
+            Position = position;
+        }
     }
 }
--- Donjon/Map.cs
+++ Donjon/Map.cs
@@ -11,6 +11,14 @@
             this.width = width;
             this.height = height;
             this.cells = new Cell[width, height];
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    var position = new Position(x, y);
+                    cells[x, y] = new Cell(position);
+                }
+            }
         }
     }
 }
