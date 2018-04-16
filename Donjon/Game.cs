83552f04396a0b68bb3a81d29b2146f5611e2d4e
Patrick <lmpatrick.frank@gmail.com>
2018-04-16 11:54:02 +0200

    New methosa in Game

--- Donjon/Game.cs
+++ Donjon/Game.cs
@@ -11,7 +11,39 @@ namespace Donjon
 
         internal void run()
         {
+            init(); // Could be in construcor
+            bool gameInProgress = false;
+            do
+            {
+                Draw();
+                var key = Console.ReadKey(intercept: true).Key;
+                switch (key)
+                {
+                    case ConsoleKey.UpArrow: MoveHero(); break;
+                    case ConsoleKey.DownArrow: MoveHero(); break;
+                    case ConsoleKey.LeftArrow: MoveHero(); break;
+                    case ConsoleKey.RightArrow: MoveHero(); break;
+                    default: break;
+                }
+
+            } while (gameInProgress);
+
             Console.WriteLine("Game Over");
+        }
+
+        private void MoveHero()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Draw()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void init()
+        {
+            throw new NotImplementedException();
         }
     }
 }
